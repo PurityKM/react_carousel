4dc8e38af1bb0514a904000e61a79deec5195dab
PurityKM <irungu1210@gmail.com>
2023-02-17 00:35:59 +0300

    React Carousel commits

--- src/App.js
+++ src/App.js
@@ -4,10 +4,26 @@ import Card from "./Card";
 import './App.css';
 
 export default function App() {
+  const breakPoints = [
+    { width: 500, itemToShow: 1 },
+    { width: 768, itemToShow: 2 },
+    { width: 1200, itemToShow: 3 },
+    { width: 1500, itemToShow: 4 }
+  ];
+  
+
+
   return (
     <div className="App">
-      <Carousel>
-        <Card />
+      <Carousel breakPoints={breakPoints}>
+        <Card number="1"/>
+        <Card number="2"/>
+        <Card number="3"/>
+        <Card number="4"/>
+        <Card number="5"/>
+        <Card number="6"/>
+        <Card number="7"/>
+        <Card number="8"/> 
       </Carousel>
     </div>
   );
